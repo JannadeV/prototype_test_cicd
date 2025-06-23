edaf1545c8f5e6b073cc4c74356b6cfef1c827e9
JannadeV <s1124226@student.hsleiden.nl>
2025-06-23 13:52:25 +0200

    base of drag-and-drop functionality, like https://dev.to/wolfmath/super-simple-drag-n-drop-from-scratch-in-react-without-dependencies-1j84

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,12 +2,14 @@
 'use client'
 
 import { useState } from "react";
+import TestDrop from "../components/testDrop.jsx";
 import PhaseBucket from "../components/phaseBucket.jsx";
 
 export default function Home() {
 
+  const [dragging, setDragging] = useState();
   const phases = ["build", "test", "deploy"];
-  const [tests, settests] = useState([
+  const [tests, setTests] = useState([
     { id: 1, phase: phases[0], name: "package"},
     { id: 2, phase: phases[0], name: "build-dev"},
     { id: 3, phase: phases[1], name: "unittest"},
@@ -15,17 +17,32 @@ export default function Home() {
     { id: 5, phase: phases[2], name: "deploy"},
   ]);
 
+  const handleDragStart = (e) => {
+    setDragging(e.target);
+  };
+  const handleDragEnter = (e, phase) => {
+    setTests([...tests, (tests[dragging.id - 1].phase = phase)]);
+  }
+
   return (
 
     <div className="flex flex-col">
-      <section className="flex flex-row">
+      <section className="flex flex-col">
         {phases.map((phase) => (
           <PhaseBucket
             key={phase}
             phaseName={phase} 
             tests={tests
                     .filter((test) => test.phase === phase)
+                    .map((item) => (
+                      <div draggable
+                            onDragStart={(e) => handleDragStart(e)}
+                            id={item.id}>
+                        <TestDrop testName={item.name}/>
+                      </div>
+                    ))
                   }
+            onDragEnter={(e) => handleDragEnter(e, phase)}
           />
         ))}
       </section>
--- src/components/phaseBucket.jsx
+++ src/components/phaseBucket.jsx
@@ -1,17 +1,12 @@
 
 import TestDrop from "../components/testDrop.jsx";
 
-export default function PhaseBucket({phaseName, tests}) {
+export default function PhaseBucket({phaseName, tests, onDragEnter}) {
     return (
-        <div className="min-h-50 min-w-30 bg-gray-100 px-4 py-3 m-2 border rounded-2xl shadow-md">
+        <div className="min-h-50 min-w-30 bg-gray-100 px-4 py-3 m-2 border rounded-2xl shadow-md" onDragEnter={onDragEnter}>
             <h3 className="font-semibold"> {phaseName} </h3>
             <section className="flex flex-row">
-                {tests.map((item) => (
-                    <TestDrop
-                        key={item.id}
-                        testName={item.name}
-                    />
-                )) }
+                {tests}
             </section>
         </div>
     );
