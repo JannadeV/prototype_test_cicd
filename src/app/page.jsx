2053d71099e5f179c851e58a60b290efa4616221
JannadeV <s1124226@student.hsleiden.nl>
2025-06-23 10:39:46 +0200

    get html typing suggestions like emmet abbreviations; make own front page

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import TestDrop from "../components/testDrop.jsx"
+
+export default function Home() {
+  return (
+
+    <TestDrop 
+      testName={ 'default Test' } />
+      
+  );
+}
